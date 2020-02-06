7e966d5daf54e4fd414ec96be5a9e86b5fe45b43
Cusmash <iori_cusmash@hotmail.com>
2020-02-06 16:38:15 -0600

    cambios controller

--- ProsisMTTO/Controllers/LanesCatalogsController.cs
+++ ProsisMTTO/Controllers/LanesCatalogsController.cs
@@ -29,15 +29,11 @@ namespace ProsisMTTO.Controllers
             return await _context.LanesCatalogs.ToListAsync();
         }
 
-        //[HttpGet("{param1}/{param2}")]
-        //[Route("api/params/")]
-        //public HttpResponseMessage Get
-
-        // GET: api/LanesCatalogs/5
-        [HttpGet("{param1}/{param2}")]
-        public async Task<ActionResult<List<LanesCatalog>>> GetLanesCatalog(string param1, int param2)
+        [HttpGet("{id}")]
+        [Route("api/LanesCatalogs/")]
+        public async Task<ActionResult<LanesCatalog>> GetLanesCatalog(string id)
         {
-            var lanesCatalog = await _context.LanesCatalogs.Where(x => x.SquaresCatalogId == param1 && x.LaneType == param2).ToListAsync();
+            var lanesCatalog = await _context.LanesCatalogs.FindAsync(id);
 
             if (lanesCatalog == null)
             {
@@ -47,11 +43,71 @@ namespace ProsisMTTO.Controllers
             return lanesCatalog;
         }
 
-        // PUT: api/LanesCatalogs/5
+
+        // GET: api/LanesCatalogs/5
+        [HttpGet("params/{param1}/{param2?}")]
+        [Route("api/LanesCatalogs/params")]
+        public async Task<ActionResult<List<LanesCatalog>>> GetLanesCatalog(string param1, int? param2)
+        {
+            try
+            {
+                if(param2 == null)
+                {
+                    var lanesCatalog = await _context.LanesCatalogs.Where(x => x.SquaresCatalogId == param1).ToListAsync();
+
+                    if (lanesCatalog == null)
+                    {
+                        return NotFound();
+                    }
+
+                    return lanesCatalog;
+                }
+                else
+                {
+                    var lanesCatalog = await _context.LanesCatalogs.Where(x => x.SquaresCatalogId == param1 && x.LaneType == param2).ToListAsync();
+
+                    if (lanesCatalog == null)
+                    {
+                        return NotFound();
+                    }
+
+                    return lanesCatalog;
+                }
+            }
+            catch
+            {
+                return NotFound();
+            }
+            //List<LanesCatalog> lanesCatalogs = null;
+            //try
+            //{
+            //    if (id != null)
+            //        {
+            //        //var lanesCatalog = await _context.LanesCatalogs.FindAsync(id);
+            //        lanesCatalogs = new List<LanesCatalog>
+            //        {
+            //            id
+            //        };
+
+            //            return lanesCatalogs;
+            //        }
+            //    else
+            //    {
+            //       var lanesCatalog = await _context.LanesCatalogs.Where(x => x.SquaresCatalogId == param1 && x.LaneType == param2).ToListAsync();
+            //        return lanesCatalog;
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    return NotFound();
+            //}
+                      
+        }
+
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutLanesCatalog(string id, LanesCatalog lanesCatalog)
+        public async Task<IActionResult> PutLanesCatalog(string id, [FromBody] LanesCatalog lanesCatalog)
         {
             if (id != lanesCatalog.CapufeLaneNum)
             {
@@ -75,6 +131,30 @@ namespace ProsisMTTO.Controllers
                     throw;
                 }
             }
+
+            //var _laneCatalog = await _context.LanesCatalogs.FirstOrDefaultAsync(x => x.CapufeLaneNum.Equals(id));
+
+            //_context.Entry(_laneCatalog).CurrentValues.SetValues(lanesCatalog);
+
+            //try
+            //{
+            //    await _context.SaveChangesAsync();
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw;
+            //}
+            //catch (DbUpdateConcurrencyException)
+            //{
+            //    if (!LanesCatalogExists(id))
+            //    {
+            //        return NotFound();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
 
             return NoContent();
         }
---
+++ ProsisMTTO/Controllers/SparePartsCatalogsController.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProsisMTTO.Context;
+using ProsisMTTO.Entities;
+
+namespace ProsisMTTO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SparePartsCatalogsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SparePartsCatalogsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/SparePartsCatalogs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SparePartsCatalog>>> GetSparePartsCatalogs()
+        {
+            return await _context.SparePartsCatalogs.ToListAsync();
+        }
+
+        // GET: api/SparePartsCatalogs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SparePartsCatalog>> GetSparePartsCatalog(string id)
+        {
+            var sparePartsCatalog = await _context.SparePartsCatalogs.FindAsync(id);
+
+            if (sparePartsCatalog == null)
+            {
+                return NotFound();
+            }
+
+            return sparePartsCatalog;
+        }
+
+        [HttpGet("params/{param1?}/{param2?}")]
+        public async Task<ActionResult<List<object>>> GetSparePartsCatalog(string? param1, string? param2)
+        {
+            try
+            {
+                var sparePartsCatalog = new List<object>();
+                if (param1 == "null" && param2 == "null")
+                {
+                    return NotFound();
+                }
+
+                else if (param2 == "null")
+                {
+                    sparePartsCatalog = await _context.SparePartsCatalogs
+                       .Where(x => x.PieceYear == param1)
+                       .Select(x => new
+                       {
+                           Id = x.NumPart,
+                           NombreRefaccion = x.Name,
+                           Marca = x.Brand,
+                           Precio = x.Price,
+                           Unidad = x.Unit
+                       })
+                      .ToListAsync<object>();
+
+                    if (sparePartsCatalog == null)
+                    {
+                        return NotFound();
+                    }
+
+                    return sparePartsCatalog;
+
+                }
+
+                else if (param1 == "null")
+                {
+                    sparePartsCatalog = await _context.SparePartsCatalogs
+                        .Where(x => x.Brand == param2)
+                        .Select(x => new
+                        {
+                            Id = x.NumPart,
+                            NombreRefaccion = x.Name,
+                            Marca = x.Brand,
+                            Precio = x.Price,
+                            Unidad = x.Unit
+                        })
+                        .ToListAsync<object>();
+
+                    if (sparePartsCatalog == null)
+                    {
+                        return NotFound();
+                    }
+
+                    return sparePartsCatalog;
+                }
+
+                else
+                {
+                    sparePartsCatalog = await _context.SparePartsCatalogs
+                        .Where(x => x.PieceYear == param1 && x.Brand == param2)
+                        .Select(x => new
+                        {
+                            Id = x.NumPart,
+                            NombreRefaccion = x.Name,
+                            Marca = x.Brand,
+                            Precio = x.Price,
+                            Unidad = x.Unit
+                        })
+                        .ToListAsync<object>();
+
+                    if (sparePartsCatalog == null)
+                    {
+                        return NotFound();
+                    }
+
+                    return sparePartsCatalog;
+                }
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        // PUT: api/SparePartsCatalogs/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSparePartsCatalog(string id, SparePartsCatalog sparePartsCatalog)
+        {
+            if (id != sparePartsCatalog.NumPart)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sparePartsCatalog).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SparePartsCatalogExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/SparePartsCatalogs
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<SparePartsCatalog>> PostSparePartsCatalog(SparePartsCatalog sparePartsCatalog)
+        {
+            _context.SparePartsCatalogs.Add(sparePartsCatalog);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SparePartsCatalogExists(sparePartsCatalog.NumPart))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetSparePartsCatalog", new { id = sparePartsCatalog.NumPart }, sparePartsCatalog);
+        }
+
+        // DELETE: api/SparePartsCatalogs/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<SparePartsCatalog>> DeleteSparePartsCatalog(string id)
+        {
+            var sparePartsCatalog = await _context.SparePartsCatalogs.FindAsync(id);
+            if (sparePartsCatalog == null)
+            {
+                return NotFound();
+            }
+
+            _context.SparePartsCatalogs.Remove(sparePartsCatalog);
+            await _context.SaveChangesAsync();
+
+            return sparePartsCatalog;
+        }
+
+        private bool SparePartsCatalogExists(string id)
+        {
+            return _context.SparePartsCatalogs.Any(e => e.NumPart == id);
+        }
+    }
+}
